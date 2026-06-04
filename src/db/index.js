f41d2591b84ae6b299eb512b8d25460875b2b3a0
AakashSudheendra <aakash151205@gmail.com>
2026-06-04 18:50:53 +0530

    Updated Index.js

--- src/db/index.js
+++ src/db/index.js
@@ -1,21 +1,15 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 
-///connecting database evertime think two problems
-// 1)database is in other country it takes time to Comment
-// 2)it may provide errors
 
-const connectDb = async () => {
-  try {
-    const connectionInstance = await mongoose.connect(
-      `${process.env.MONGODB_URL}/${DB_NAME}`
-    );  
-    console.log(
-      `MongoDB Connected with Host ${connectionInstance.connection.host}`
-    );
-  } catch (error) {
-    console.log("MongoDB Connection Error is Comming.....", error);
-    process.exit(1)
-  }
-};
-export {connectDb}
+const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
+    } catch (error) {
+        console.log("MONGODB connection FAILED ", error);
+        process.exit(1)
+    }
+}
+
+export default connectDB
